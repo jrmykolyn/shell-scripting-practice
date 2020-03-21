7e8b0d6b7e12bc6796c86c5f45682dd2802f4d49
Jesse Mykolyn <jrmykolyn@gmail.com>
2020-03-20 20:41:30 -0400

    Describe features and functionality of program

--- calc.sh
+++ calc.sh
@@ -1,1 +1,11 @@
 #! /usr/bin/env bash
+
+# When the program is first loaded, display a greeting to the user.
+# Then, display a menu that outlines the possible operations:
+  # Add
+  # Subtract
+  # Exit
+# Then, capture the user selection.
+# If the selection matches a supported operation, execute the operation.
+# If the selection does not match a support operation, display an error message.
+# When the operation is complete, redisplay the menu.
